26f47dde6aec6dc0c26a2e382852e6f873039253
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-26 23:43:11 -0400

    get user input for guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please enter a number between 1 and 100: ");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read user input");
+
+    println!("You guessed {}", guess);
 }
