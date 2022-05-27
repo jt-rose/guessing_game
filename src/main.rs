0e27e7137fd95d53d77791f4da07fa664301a3de
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-27 00:31:15 -0400

    remove console log of secret number and add cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1 .. 101);
-
-    println!("The not so secret number has been generated as: {}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
     let mut guess_count: i32 = 0;
     loop {
@@ -15,27 +13,27 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess).expect("Failed to read user input");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read user input");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue
+            Err(_) => continue,
         };
 
         println!("You guessed {}", guess);
         guess_count += 1;
 
         match guess.cmp(&secret_number) {
-            Ordering::Less =>
-                println!("Too small!"),
-            Ordering::Greater =>
-                println!("Too big!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("Good Guess! You got it");
                 break;
             }
-            }
         }
+    }
 
-        println!("You guessed the answer in {} turns", guess_count);
+    println!("You guessed the answer in {} turns", guess_count);
 }
