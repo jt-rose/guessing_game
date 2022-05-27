c30bb5ff80a71d07b16d6c75e733528341d5a2d4
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-26 23:56:06 -0400

    generate secret number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1 .. 101);
+
+    println!("The not so secret number has been generated as: {}", secret_number);
 
     println!("Please enter a number between 1 and 100: ");
 
